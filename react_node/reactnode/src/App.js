605bcadc2a951df4e2755b0f2d7a8626a58ed2c3
Suraj Kumar <surajkumar06174@gmail.com>
2023-05-01 18:27:25 +0530

    modified:   react_node/reactnode/src/App.js 	new file:   react_node/reactnode/src/newcomponent.css 	new file:   react_node/reactnode/src/newcomponent.js

--- react_node/reactnode/src/App.js
+++ react_node/reactnode/src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
+import New from './newcomponent';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ export function App2() {
       <label>Username </label><input type="text" value="" placeholder="Enter Your Name"></input><br></br>
       <label>Password </label><input type="text" value="" placeholder="Enter Your Password"></input><br></br>
       <button>Sign In</button>
+      <New />
     </div>
   );
 }
---
+++ react_node/reactnode/src/newcomponent.js
@@ -0,0 +1,12 @@
+import './newcomponent.css'
+
+function New (){
+    return(
+        <>
+        <h1 className="nw">Hello Folks</h1>
+        <h3 className="nww">This is a new component</h3>
+        </>
+    )
+}
+
+export default New
