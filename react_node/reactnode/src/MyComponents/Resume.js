bcc19f41fb685fd77f773637db0038101f7ffee1
Suraj Kumar <surajkumar06174@gmail.com>
2023-05-03 17:32:46 +0530

    modified:   react_node/reactnode/src/ComponentCss/Resme.css 	modified:   react_node/reactnode/src/MyComponents/Resume.js 	modified:   react_node/reactnode/src/index.js

--- react_node/reactnode/src/MyComponents/Resume.js
+++ react_node/reactnode/src/MyComponents/Resume.js
@@ -3,7 +3,7 @@ import '../ComponentCss/Resume.css'
 export default function Resume(){
     return(
         <>
-        <h1>Resume</h1>
+        <h1 id="hd">Resume</h1>
         <h2>Suraj Gupta</h2>
         <h4>Contact: 7367089724</h4>
         <h4>Mail: surajkr97@icloud.com</h4>
