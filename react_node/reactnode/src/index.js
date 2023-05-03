38feaaddc5d390aa3c93917df4b69de7c32b411b
Suraj Kumar <surajkumar06174@gmail.com>
2023-05-03 17:57:40 +0530

    Changes to be committed: 	renamed:    react_node/reactnode/src/ComponentCss/Resme.css -> react_node/reactnode/src/ComponentCss/Resume.css 	modified:   react_node/reactnode/src/index.js

--- react_node/reactnode/src/index.js
+++ react_node/reactnode/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { App2 } from './App';
+import Resume from './MyComponents/Resume';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <App />
     <App2 />
+    <Resume />
   </>
 );
