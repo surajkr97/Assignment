6be949ebcbb85c6a7fd114b6f76e541f3d1bd15e
Suraj Kumar <surajkumar06174@gmail.com>
2023-05-04 15:20:03 +0530

    modified:   react_node/reactnode/src/App.css 	modified:   react_node/reactnode/src/App.js 	modified:   react_node/reactnode/src/index.js

--- react_node/reactnode/src/App.js
+++ react_node/reactnode/src/App.js
@@ -1,28 +1,25 @@
 // import logo from './logo.svg';
 import './App.css';
+import Resume from './MyComponents/Resume';
 import New from './MyComponents/new';
 
-function App() {
-  return (
+export default function App(){
+  return(
     <>
-      <h1>Form</h1>
-      <hr width="80%"></hr>
+    <New />
+    </>
+  )
+}
+
+export function App1(){
+  return(
+    <>
+      <Resume />
     </>
   );
 }
 
-export function App2() {
-  return (
-    <div id="hero">
-      <label>Username </label><input type="text" value="" placeholder="Enter Your Name"></input><br></br>
-      <label>Password </label><input type="text" value="" placeholder="Enter Your Password"></input><br></br>
-      <button>Sign In</button>
-      <New />
-    </div>
-  );
-}
 
-export default App;
 
 //reusability
 //jsx : javascript xml
--- react_node/reactnode/src/index.js
+++ react_node/reactnode/src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import { App2 } from './App';
-import Resume from './MyComponents/Resume';
+// import App from './App';
+import { App1 } from './App'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <App />
-    <App2 />
-    <Resume />
+  {/* <App /> */}
+  <App1 />
   </>
 );
