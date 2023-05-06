16f8f8ba86bca6e27ac4ee5ca43715b981c31c4f
Suraj Kumar <surajkumar06174@gmail.com>
2023-05-06 18:30:13 +0530

    modified:   react_node/reactnode/src/App.js 	new file:   react_node/reactnode/src/ComponentCss/Expense.css 	new file:   react_node/reactnode/src/MyComponents/Expense.js 	deleted:    react_node/reactnode/src/img/SittingCartoon.png.webp 	modified:   react_node/reactnode/src/index.js

--- react_node/reactnode/src/App.js
+++ react_node/reactnode/src/App.js
@@ -1,27 +1,27 @@
-// import logo from './logo.svg';
 import './App.css';
-import Resume, { Resume1, Resume2, Resume3 } from './MyComponents/Resume';
-import New from './MyComponents/new';
+import { ExpenseTrack } from './MyComponents/Expense';
 
-export default function App(){
-  return(
-    <>
-    <New />
-    </>
-  )
-}
+// import Resume, { Resume1, Resume2, Resume3 } from './MyComponents/Resume';
+// import New from './MyComponents/new';
 
-export function App1(){
-  return(
-    <>
-      <Resume />
-      <Resume1 />
-      <Resume2 />
-      <Resume3 />
-    </>
-  );
-}
+// export default function App(){
+//   return(
+//     <>
+//     <New />
+//     </>
+//   )
+// }
 
+// export function App1(){
+//   return(
+//     <>
+//       <Resume />
+//       <Resume1 />
+//       <Resume2 />
+//       <Resume3 />
+//     </>
+//   );
+// }
 
 
 //reusability
@@ -29,3 +29,21 @@ export function App1(){
 //xml : xtensible markup language
 //virtual dom : copy of real dom
 //React.fragment used to create group by avoiding div (<> </>)
+
+
+export function ExpenseApp(){
+    let ob = {
+        "dt1": new Date(2020, 1, 19), "nm1": "Car Insurance", "pr1": "$200",
+        "dt2": new Date(2021, 8, 24), "nm2": "T-Shirt for Goa Trip", "pr2": "$80",
+        "dt3": new Date(2022, 7, 11), "nm3": "Shoes", "pr3": "$700",
+        "dt4": new Date(2023, 2, 25), "nm4": "Goa Trip", "pr4": "$2600"
+    }
+    return(
+        <>
+        <ExpenseTrack date={ob.dt1} exp={ob.nm1} price={ob.pr1}/>
+        <ExpenseTrack date={ob.dt2} exp={ob.nm2} price={ob.pr2}/>
+        <ExpenseTrack date={ob.dt3} exp={ob.nm3} price={ob.pr3}/>
+        <ExpenseTrack date={ob.dt4} exp={ob.nm4} price={ob.pr4}/>
+        </>
+    );
+}
---
+++ react_node/reactnode/src/MyComponents/Expense.js
@@ -0,0 +1,21 @@
+import "../ComponentCss/new.css"
+
+export function ExpenseTrack(props) {
+    let month = props.date.toLocaleString('en - US', { month: 'long' })
+    let year = props.date.getFullYear();
+    let day = props.date.toLocaleString('en-US', { day: '2-digit' })
+    return (
+        <div className="expense">
+            <div className='dstyle'>
+                <div><b>{month}</b></div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
+
+            <div className="exp_desc">
+                <h2>{props.exp}</h2>
+                <div className="exp_pr">{props.price}</div>
+            </div>
+        </div>
+    )
+}
