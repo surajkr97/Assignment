c436e4216b1a2cf0cd222b51b33f08d84c5be776
Suraj Kumar <surajkumar06174@gmail.com>
2023-05-08 16:20:44 +0530

    modified:   react_node/reactnode/src/index.js

--- react_node/reactnode/src/index.js
+++ react_node/reactnode/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Factorial from './MyComponents/Factorial';
+import HideShow from './MyComponents/HideShowButton'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-  <Factorial />
+  <HideShow />
   </>
 );
