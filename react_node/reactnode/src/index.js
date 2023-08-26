65972671f213656950cb80a03504faedc83d38df
Suraj Kumar <surajkumar06174@gmail.com>
2023-08-26 12:56:47 +0530

    modified:   react_node/.DS_Store 	modified:   react_node/reactnode/src/ComponentCss/Resume.css 	modified:   react_node/reactnode/src/index.js

--- react_node/reactnode/src/index.js
+++ react_node/reactnode/src/index.js
@@ -1,13 +1,18 @@
 // import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Nvbar from './MyComponents/bootstrap';
+// import Nvbar from './MyComponents/bootstrap';
+import Resume, { Resume1, Resume2, Resume3 } from './MyComponents/Resume';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <Nvbar />
+    {/* <Nvbar /> */}
+    <Resume/>
+    <Resume1/>
+    <Resume2/>
+    <Resume3/>
   </>
 );
